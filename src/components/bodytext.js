62ae30466e8aa7d8820b86b7d477af3f2396e3c2
capriblue <capri4wrk@gmail.com>
2021-03-29 19:46:58 +0900

    コードのリファクタリングをしました。

--- src/components/bodytext.js
+++ src/components/bodytext.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "gatsby";
 import { MDXRenderer } from "gatsby-plugin-mdx";
 import * as style from "./bodytext.module.css";
 export function Body(props) {
