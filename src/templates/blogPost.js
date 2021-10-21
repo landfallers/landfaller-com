d907ff2ad30223b4888ea41fed0d853e30a00665
capriblue <capri4wrk@gmail.com>
2021-10-21 14:02:19 +0900

    埋め込みに対応

--- src/templates/blogPost.js
+++ src/templates/blogPost.js
@@ -6,10 +6,12 @@ import { graphql, Link } from "gatsby";
 import { Layout } from "../components/layout";
 import { Seo } from "../components/seo";
 import { Body } from "../components/bodytext.js";
+import Iframely from "../components/iframely.js";
 export default function Article({ data, location }) {
   const { frontmatter } = data.mdx;
   return (
     <Layout>
+      <Iframely/>
       <Seo
       pagetitle={frontmatter.title}
       pagedesc={frontmatter.preface}
